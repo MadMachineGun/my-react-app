fdfef35624cb18ec0a8a0a921818e09b92438e6e
Yurii Kazakov <gfnvortex@gmail.com>
2023-11-02 16:11:23 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import Header from "./components/Header/Header";
 import ClassCounter from "./components/ClassCounter/ClassCounter";
 
 function App() {
-  return (
-    <>
-      <Header/>
-        <ClassCounter/>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <ClassCounter/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <span>Menu</span>
+            <span>Logo</span>
+            <span>User Photo</span>
+        </div>
+    );
+}
+
+export default Header;
